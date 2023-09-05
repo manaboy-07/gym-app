0f2e239a79d149111f502f7645abfe08dd2ad3d0
manaboy-07 <manassehoruebor@gmail.com>
2023-09-05 15:43:49 +0100

    gym layout done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
 import './App.css';
 import {Route, Routes} from 'react-router-dom'
-import {Box } from '@mui/material'
+import { Box } from '@mui/material'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 function App() {
   return (
     <Box width='400px' sx={{width: {xl: '1488px'}}} m='auto'>
@@ -10,6 +14,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/exercise/:id' element={<ExerciseDetail />} />
      </Routes>
+     <Footer/>
     </Box>
   );
 }
