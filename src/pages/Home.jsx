a12b81e0aeae9b3953782bf0c17f41050187f62b
manaboy-07 <manassehoruebor@gmail.com>
2023-09-05 16:24:20 +0100

    done with one component of the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 /** @format */
 
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import { Stack } from "@mui/material";
+import Exercises from '../components/Exercises'
+import HeroBanner from '../components/HeroBanner'
+import { Stack, Box } from "@mui/material";
+import SearchExercises from '../components/SearchExercises'
 import Logo from "../assets/images/Logo.png";
 const Home = () => {
   return (
-    <>
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
      <h1>Home</h1>
-    </>
+    </Box>
   );
 };
 
